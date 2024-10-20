1d68a6c3dfa84ad7e0fc3c4919494fe198f5c95d
Timothy Sturges <tos3454@rit.edu>
2024-10-20 19:16:14 -0400

    Added a pandas package to manage the csv file, could be useful

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 import sys, os
 import argparse
 
+import pandas as pd
+
+
 def main():
 	parser = argparse.ArgumentParser("movies")
-	parser.add_argument("-create", 
+	parser.add_argument('csv_file')
+	args = parser.parse_args()
+	df = pd.read_csv(args.csv_file)
+	print(df.head)
+
+if __name__ == '__main__':
+	main()
+
