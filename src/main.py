2931608ae5c0fa044780975326851ea1ad6a0c69
Timothy Sturges <tos3454@rit.edu>
2024-10-20 19:34:52 -0400

    Trying out a basic line by line read

--- src/main.py
+++ src/main.py
@@ -5,11 +5,15 @@ import pandas as pd
 
 
 def main(): # run with parameters IMDB-Movie-Data.csv and it will read it
-	parser = argparse.ArgumentParser("movies")
-	parser.add_argument('csv_file')
-	args = parser.parse_args()
-	df = pd.read_csv(args.csv_file)
-	print(df.head)
+	#	parser = argparse.ArgumentParser("movies")
+	#parser.add_argument('csv_file')
+	#args = parser.parse_args()
+	#df = pd.read_csv(args.csv_file)
+	#print(df)
+	with open("IMDB-Movie-Data.csv") as csv:
+		for line in csv:
+			print(line)
+
 
 if __name__ == '__main__':
 	main()
