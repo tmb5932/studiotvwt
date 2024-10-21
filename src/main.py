fb7ee886f24b1d3d9bab9606f66a2df2079bbe82
Timothy Sturges <tos3454@rit.edu>
2024-10-20 20:02:37 -0400

    Reads a csv  file and makes an sql statement in the form of a str called sql_statement, for each row, no mpaa though

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys, os
 import argparse
+import csv
+from _csv import reader
 
 import pandas as pd
 
@@ -10,9 +12,25 @@ def main(): # run with parameters IMDB-Movie-Data.csv and it will read it
 	#args = parser.parse_args()
 	#df = pd.read_csv(args.csv_file)
 	#print(df)
-	with open("IMDB-Movie-Data.csv") as csv:
-		for line in csv:
-			print(line)
+
+	with open("IMDB-Movie-Data.csv") as c:
+		csv_reader = csv.reader(c)
+
+		movieid = 1
+		for row in csv_reader:
+			title = row[1]
+			genre = row[2]
+			genres = genre.split(",")
+			genstr = ""
+			for genre in genres:
+				genstr += genre + " "
+			director = row[4]
+			year = row[6]
+			runtimemins = row[7]
+			rating = row[8]
+			sql_statement = ("INSERT INTO movie (movieid, title, runtime) VALUES (" + str(movieid) + ", " + genstr + ", " + runtimemins)
+			movieid = movieid + 1
+
 
 
 if __name__ == '__main__':
