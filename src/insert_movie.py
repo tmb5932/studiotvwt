d032ad70be83a59ce95530ca1ee82ccb7bdf9a9a
Timothy Sturges <tos3454@rit.edu>
2024-10-30 16:41:22 -0400

    Completed insert_movie.py without ssh

--- src/insert_movie.py
+++ src/insert_movie.py
@@ -13,31 +13,40 @@ def main(): # run with parameters IMDB-Movie-Data.csv and it will read it
 	#df = pd.read_csv(args.csv_file)
 	#print(df)
 
-	with open("IMDB-Movie-Data.csv") as c:
+	with open("netflix_titles.csv", encoding="utf-8") as c:
 		csv_reader = csv.reader(c)
 
 		# skips first row / inits movieid
 		header = True
 		movieid = 1
-		print("INSERT INTO movie (movieid, title, runtime) VALUES " , end="")
+		# print("INSERT INTO movie (movieid, title, runtime, mpaa) VALUES " , end="")
 		for row in csv_reader:
 			if header:
 				header= False
 				continue
 
+			if (row[1] == "Movie"):
+				title = row[2]
+				runtimemins = row[9]
+				runtimemins = runtimemins.strip().split(",")
+				runtimemins = runtimemins[0]
+				rating = row[8]
+				if(rating == "TV-MA"):
+					rating = "R"
+				if(rating == "TV-14"):
+					rating = "PG-13"
+				if(rating == "TV-G"):
+					rating = "G"
+				if(rating == "TV_PG"):
+					rating = "PG"
+				if(rating == "TV-Y7"):
+					rating = "G"
 
-			title = row[1]
-			genre = row[2]
-			genres = genre.split(",")
-			genstr = ""
-			for genre in genres:
-				genstr += genre + " "
-			runtimemins = row[7]
+				sql_statement = ("INSERT INTO movie (movieid, title, runtime) VALUES (" + str(movieid) + ", '" + title + "', " + runtimemins + ", '" + rating + "'"  + ")")
 
-			# sql_statement = ("INSERT INTO movie (movieid, title, runtime) VALUES (" + str(movieid) + ", " + title + ", " + runtimemins + ")")
-			movieid = movieid + 1
-			# print(sql_statement)
-			print("(" + str(movieid) + ", '" + title + "', " + runtimemins + "), ", end="")
+				print(sql_statement)
+				#print("(" + str(movieid) + ", '" + title + "', " + runtimemins + ", '" + rating + "'), ", end="")
+				movieid = movieid + 1
 
 
 
