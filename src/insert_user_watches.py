0d6e5bc0311bb2ea34145746a9adc887d17d6ede
Timothy Sturges <tos3454@rit.edu>
2024-10-31 18:16:51 -0400

    Working on insert_user_watches.py currently, not working just yet

---
+++ src/insert_user_watches.py
@@ -0,0 +1,79 @@
+import os
+import csv
+import time
+import random
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+from dotenv import load_dotenv
+
+
+def makeSQLStatement():
+    statements = []
+    for movie in range(0,999):
+        user = random.randint(0, 502)
+        year = str(random.randint(1950, 2024))
+        month = str(random.randint(1, 12))
+        day = str(random.randint(1, 28))
+        if int(month) < 10:
+            month = "0" + month
+        if int(day) < 10:
+            day = "0" + day
+        hour = str(random.randint(1, 24))
+        if int(hour) < 10:
+            hour = "0" + hour
+        minutes = str(random.randint(0, 59))
+        if int(minutes) < 10:
+            minutes = "0" + minutes
+        seconds = str(random.randint(0, 59))
+        if int(seconds) < 10:
+            seconds = "0" + seconds
+        statements.append((movie, user, year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds))
+    return statements
+
+def sshTunnel():
+    try:
+        # load_dotenv()
+        username = os.getenv("USERNAME")
+        password = os.getenv("PASSWORD")
+        dbName = "p320_11"
+
+        with SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                ssh_username=username,
+                                ssh_password=password,
+                                remote_bind_address=('127.0.0.1', 5432)) as server:
+            server.start()
+            print("SSH tunnel established")
+
+            params = {
+                'database': dbName,
+                'user': username,
+                'password': password,
+                'host': 'localhost',
+                'port': server.local_bind_port
+            }
+            conn = psycopg2.connect(**params)
+            curs = conn.cursor()
+            print("Database connection established")
+
+            insert_query = """
+                INSERT INTO userwatches (movieid, userid, watchdate) VALUES (%s, %s, %s)
+                """
+            statements = makeSQLStatement()
+            curs.executemany(insert_query, statements)
+            conn.commit()
+            print("All movie-producer relations inserted successfully!")
+            conn.close()
+
+
+    except:
+        print("Connection failed")
+
+
+
+def main():
+    sshTunnel()
+
+
+
+if __name__ == "__main__":
+    main()
