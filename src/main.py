ae9a2576536fcf543b50499e72307afb00f255a9
tmb5932 <tmb5932@g.rit.edu>
2024-11-01 20:14:55 -0400

    fixed spacing

--- src/main.py
+++ src/main.py
@@ -524,82 +524,70 @@ def userrates():
 		print(red.apply("\tFailed to add rating."))
 
 def watch():
-    global logged_in, logged_in_as
-    if not logged_in:
-        print(red.apply("\tYou must be signed in to watch a movie or collection."))
-        return False
+	global logged_in, logged_in_as
+	if not logged_in:
+		print(red.apply("\tYou must be signed in to watch a movie or collection."))
+		return False
 
-    while True:
-        media_type = input('Watch a single Movie or Collection? (input "m" or "c"): ')
-        if media_type.lower() == 'q':
-            print("Watch process canceled.")
-            return
+	while True:
+		media_type = input('Watch a single Movie or Collection? (input "m" or "c"): ')
+		if media_type.lower() == 'q':
+			print("Watch process canceled.")
+			return
 
-        if media_type not in ["m", "c"]:
-            print("Invalid input. Please enter 'movie' or 'collection'.")
-        else:
-            break
+		if media_type not in ["m", "c"]:
+			print("Invalid input. Please enter 'movie' or 'collection'.")
+		else:
+			break
 
-    watch_date = datetime.now().isoformat(' ', 'microseconds')
+	watch_date = datetime.now().isoformat(' ', 'microseconds')
 
-    if media_type == "m":
-        while True:
-            media_name = input("\tEnter the movie name ( type 'q' to quit): ")
-            if media_name.lower() == 'q':
-                print("Watch process canceled.")
-                return
+	if media_type == "m":
+		while True:
+			media_name = input("\tEnter the movie name ( type 'q' to quit): ")
+			if media_name.lower() == 'q':
+				print("Watch process canceled.")
+				return
 
-            media = GET("movie", criteria=f"title = '{media_name}'")
-            if not media:
-                print(red.apply("\tMovie not found. Please enter a proper name (check for typos)."))
-                continue
-            else:
-                media_id = media[0][0]
-                break
+			media = GET("movie", criteria=f"title = '{media_name}'")
+			if not media:
+				print(red.apply("\tMovie not found. Please enter a proper name (check for typos)."))
+				continue
+			else:
+				media_id = media[0][0]
+				break
 
-    elif media_type == "c":
-        while True:
-            media_name = input("\tEnter the collection name (or type 'q' to quit): ")
-            if media_name.lower() == 'q':
-                print("Watch process canceled.")
-                return
+	elif media_type == "c":
+		while True:
+			media_name = input("\tEnter the collection name (or type 'q' to quit): ")
+			if media_name.lower() == 'q':
+				print("Watch process canceled.")
+				return
 
-            media = GET("collection", criteria=f"name = '{media_name}'")
-            if not media:
-                print(red.apply("\tCollection not found. Please enter a proper name (check for typos)."))
-                continue
-            else:
-                collection_id = media[0][1]
-                movies = GET("collectionstores", col="movieid", criteria=f"collectionid = {collection_id}")
+			media = GET("collection", criteria=f"name = '{media_name}'")
+			if not media:
+				print(red.apply("\tCollection not found. Please enter a proper name (check for typos)."))
+				continue
+			else:
+				collection_id = media[0][1]
+				movies = GET("collectionstores", col="movieid", criteria=f"collectionid = {collection_id}")
 
-                for movie in movies:
-                    movie_id = movie[0]
-                    movie_name = GET("movie", col="title", criteria=f"movieId = {movie_id}")
-                    entry = {"movieId": movie_id, "userId": logged_in_as, "watchDate": watch_date}
-                    post_result = POST("userwatches", entry)
-                    print(green.apply(f"\tMovie marked as watched: {movie_name}."))
+				for movie in movies:
+					movie_id = movie[0]
+					movie_name = GET("movie", col="title", criteria=f"movieId = {movie_id}")
+					entry = {"movieId": movie_id, "userId": logged_in_as, "watchDate": watch_date}
+					post_result = POST("userwatches", entry)
+					print(green.apply(f"\tMovie marked as watched: {movie_name}."))
 
-                print(green.apply(f"\tEntire collection '{media_name}' marked as watched."))
-                return
+				print(green.apply(f"\tEntire collection '{media_name}' marked as watched."))
+				return
 
-    entry = {"movieId": media_id, "userId": logged_in_as, "watchDate": watch_date}
-    post_result = POST("userwatches", entry)
-    if post_result:
-        print(green.apply(f"\tMovie marked as watched: {media_name}."))
-    else:
-        print(red.apply("\tFailed to mark movie as watched."))
-
-
-
-
-
-
-
-
-
-
-
-
+	entry = {"movieId": media_id, "userId": logged_in_as, "watchDate": watch_date}
+	post_result = POST("userwatches", entry)
+	if post_result:
+		print(green.apply(f"\tMovie marked as watched: {media_name}."))
+	else:
+		print(red.apply("\tFailed to mark movie as watched."))
 
 def search_user():
 	global logged_in
